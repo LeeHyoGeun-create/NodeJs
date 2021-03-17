915ffad7e873d42ab3a38183f9255aa2ea9a6cb0
Hyogeun2 <35337131+Hyogeun2@users.noreply.github.com>
2021-03-17 22:02:06 +0900

    0317

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
 var http = require("http");
 var fs = require("fs");
-let url = require("");
+var url = require("url");
 
 var app = http.createServer(function (request, response) {
   var _url = request.url;
-  let queryData = url.parse(_url + true).qurey;
-  console.log(queryData);
-  console.log(_url + "hihi");
+  var queryData = url.parse(_url, true).query;
+  var title = queryData.id;
   if (_url == "/") {
-    _url = "/index.html";
+    title = "Welcome";
   }
   if (_url == "/favicon.ico") {
-    response.writeHead(404);
-    response.end();
-    return;
+    return response.writeHead(404);
   }
   response.writeHead(200);
-  console.log(__dirname + _url);
-  response.end(fs.readFileSync(__dirname + _url));
+  fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+    var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      `;
+    response.end(template);
+  });
 });
 app.listen(3000);
